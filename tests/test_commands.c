b504237b5f6429a2030023df6c0acdec7a1e562e
Hassan Mansoor <hassanmansoor1569@gmail.com>
2026-05-06 19:33:05 +0500

    full checks done

--- tests/test_commands.c
+++ tests/test_commands.c
@@ -1,5 +1,6 @@
-/*compiler.exe -ast tests/test_basic.c        # shows the Abstract Syntax Tree
-compiler.exe -sym tests/test_basic.c        # shows the symbol table
-compiler.exe -tac tests/test_basic.c        # shows TAC before optimization
-compiler.exe -opt tests/test_basic.c        # shows TAC after optimization
-compiler.exe -ast -sym -opt tests/test_optimizer.c   # all at once*/
+/*
+.\compiler.exe -ast tests/test_basic.c        # shows the Abstract Syntax Tree
+.\compiler.exe -sym tests/test_basic.c        # shows the symbol table
+.\compiler.exe -tac tests/test_basic.c        # shows TAC before optimization
+.\compiler.exe -opt tests/test_basic.c        # shows TAC after optimization
+.\compiler.exe -ast -sym -opt tests/test_optimizer.c   # all at once*/
